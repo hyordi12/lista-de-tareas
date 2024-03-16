56a90c4b2a166ae7228ce982cf53d72f2f321488
unknown <ucanalexis48Qgmail.com>
2024-03-16 11:28:41 -0600

    se agrega la logica de la lista

--- app/src/main/java/com/example/tareas/MainActivity.java
+++ app/src/main/java/com/example/tareas/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.tareas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private List<String> tasks = new ArrayList<>();
+    private RecyclerView recyclerViewTasks;
+    private TaskAdapter taskAdapter;
+    private EditText editTextTask;
+    private Button buttonAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewTasks = findViewById(R.id.recyclerViewTasks);
+        editTextTask = findViewById(R.id.editTextTask);
+        buttonAddTask = findViewById(R.id.buttonAddTask);
+
+        taskAdapter = new TaskAdapter(tasks);
+        recyclerViewTasks.setLayoutManager(new LinearLayoutManager(this));
+        recyclerViewTasks.setAdapter(taskAdapter);
+
+        buttonAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String task = editTextTask.getText().toString();
+                if (!task.isEmpty()) {
+                    tasks.add(task);
+                    taskAdapter.notifyItemInserted(tasks.size() - 1);
+                    editTextTask.setText("");
+                }
+            }
+        });
     }
 }
